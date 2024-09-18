df075e98ed9fc778986a1ef29f9019e7f8408ff0
cuishunbiao <860026526@qq.com>
2024-09-18 22:48:09 +0800

    code

--- src/min-cost-climbing-stairs.ts
+++ src/min-cost-climbing-stairs.ts
@@ -1,3 +1,17 @@
-function minCostClimbingStairs(cost: number[]): number {
 
+function minCostClimbingStairs(cost: number[]): number {
+    const n = cost.length;
+    if (n === 0) return 0;
+    if (n === 1) return cost[0];
+    if (n === 2) return Math.min(cost[0], cost[1])
+    const dp = new Array(n)
+    dp[0] = cost[0]
+    dp[1] = cost[1]
+    for (let i = 2; i < n; i++) {
+        dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2])
+    }
+    return Math.min(dp[n - 1], dp[n - 2])
 };
+
+const cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
+console.log(minCostClimbingStairs(cost));
