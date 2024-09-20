3b804300ae561a242a1441aae4309769d6774973
cuishunbiao <xiang3399nian@gmail.com>
2024-09-20 12:49:50 +0800

    feat: jump game ii

--- src/jump-game-ii.ts
+++ src/jump-game-ii.ts
@@ -1,28 +1,20 @@
 function jump(nums: number[]): number {
-    let n = nums.length;
-    if (n === 1) return 0; // If there's only one element, no jump is needed.
+    let jump = 0; // jump a few step
+    let current_end = 0; // current jump position end
+    let end_index = 0; // farthest jump position
 
-    let jumps = 0; // Number of jumps made
-    let current_end = 0; // End of the range you can reach with the current jump
-    let farthest = 0; // The farthest point you can reach from the current position
-
-    for (let i = 0; i < n - 1; i++) {
-        // Update the farthest point you can reach
-        farthest = Math.max(farthest, i + nums[i]);
-
-        // If you've reached the end of the current jump range, make a jump
+    // Each jump farthest from the current position
+    for (let i = 0; i < nums.length - 1; i++) {
+        // farthest jump position
+        end_index = Math.max(end_index, nums[i] + i);
+        // if current jump position end, then jump++
         if (i === current_end) {
-            jumps++;
-            current_end = farthest; // Update the end to the farthest you can reach
-
-            // If the current_end reaches or surpasses the last index, break out of the loop
-            if (current_end >= n - 1) {
-                break;
-            }
+            jump++;
+            // reset jump position end
+            current_end = end_index
         }
     }
-
-    return jumps;
+    return jump;
 };
 
 const numsArray = [2, 3, 1, 1, 4]
---
+++ src/maximum-product-subarray.ts
@@ -0,0 +1,15 @@
+function maxProduct(nums: number[]): number {
+    const len = nums.length
+    if (len === 1) return nums[0];
+
+    let result = -Infinity
+
+    // left, right
+    // 如果前面的结果，小于当前 nums[i] 的值，则直接更新  left right
+
+
+
+    return 0
+};
+const nums_arr = [2, 3, -2, 4]
+console.log(maxProduct(nums_arr));
