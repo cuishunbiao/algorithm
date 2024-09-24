45842747c0153f91832e49944a047475806b7162
cuishunbiao <860026526@qq.com>
2024-09-24 21:45:42 +0800

    code

--- src/maximum-subarray.ts
+++ src/maximum-subarray.ts
@@ -1,6 +1,11 @@
 function maxSubArray(nums: number[]): number {
-
-    return 0
+    let maximum = -Infinity;
+    let sum = 0;
+    for (let num of nums) {
+        sum = Math.max(num, sum + num)
+        maximum = Math.max(sum, maximum)
+    }
+    return maximum;
 };
 const nums8 = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
 console.log(maxSubArray(nums8));
---
+++ src/search-a-2d-matrix.ts
@@ -0,0 +1,8 @@
+function searchMatrix(matrix: number[][], target: number): boolean {
+
+
+    return true
+};
+
+const matrix = [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], target1 = 3
+console.log(searchMatrix(matrix, target1));
