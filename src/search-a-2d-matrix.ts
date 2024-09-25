28b5558e7ecd705a930b54c244208ca34acc8eed
cuishunbiao <xiang3399nian@gmail.com>
2024-09-25 13:35:42 +0800

    code

--- src/search-a-2d-matrix.ts
+++ src/search-a-2d-matrix.ts
@@ -1,5 +1,32 @@
 function searchMatrix(matrix: number[][], target: number): boolean {
+    /**
+     * 先确定行：大于每行第 1 个，并且小于下一行第 1 个，可以取出行。
+     * 拿到这一行，做二分查找
+     */
 
+    // 先确定是哪行
+    for (let i = 0; i < matrix.length; i++) {
+        debugger
+        if (matrix[i][0] <= target && target < matrix[i + 1][0]) {
+            findNumber(matrix[i])
+            break
+        }
+    }
+
+    function findNumber(nums: number[]): boolean {
+        const len = nums.length
+        let left = 0, right = len - 1, middle = Math.floor((left + right) / 2);
+
+        while (left <= right) {
+            if (target < nums[middle]) {
+                right = middle
+            } else {
+                left = middle
+            }
+        }
+
+        return false;
+    }
 
     return true
 };
