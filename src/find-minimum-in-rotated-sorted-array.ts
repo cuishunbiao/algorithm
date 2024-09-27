a5749b14f53e3740460510298173da5f9cc4270f
cuishunbiao <860026526@qq.com>
2024-09-27 20:46:11 +0800

    code

--- src/find-minimum-in-rotated-sorted-array.ts
+++ src/find-minimum-in-rotated-sorted-array.ts
@@ -1,19 +1,17 @@
+
 function findMin(nums: number[]): number {
     if (nums.length === 1) return nums[0];
-    // 如果比 right 的数小，则 right =  middle -1，如果比 right 的数大，则 left = middle+1
     let left = 0, right = nums.length - 1;
     let middle = 0
     while (left < right) {
-        debugger
         middle = Math.floor((left + right) / 2)
-        if (nums[middle] < nums[right]) {
-            right = middle - 1;
+        if (nums[middle] <= nums[right]) {
+            right = middle;
         } else {
             left = middle + 1;
         }
-        if (left === right) return nums[Math.floor((left + right) / 2)]
     }
-    return nums[middle]
+    return nums[left]
 };
 
 const nums9 = [3, 1, 2]
