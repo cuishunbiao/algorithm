7e7f25e2dd861613b0a1c9dc1bc499b511b46a21
cuishunbiao <xiang3399nian@gmail.com>
2024-09-27 13:00:54 +0800

    add

---
+++ src/capacity-to-ship-packages-within-d-days.ts
@@ -0,0 +1,6 @@
+function shipWithinDays(weights: number[], days: number): number {
+
+    return 0
+};
+const weights = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], days = 5
+console.log(shipWithinDays(weights, days));
