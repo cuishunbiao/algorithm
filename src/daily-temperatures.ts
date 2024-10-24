45d89d477b861d781cc1be77ee365a6b28117eb8
cuishunbiao <xiang3399nian@gmail.com>
2024-10-24 12:50:49 +0800

    add code1

--- src/daily-temperatures.ts
+++ src/daily-temperatures.ts
@@ -1,5 +1,21 @@
 function dailyTemperatures(temperatures: number[]): number[] {
 
+    const n = temperatures.length;
+    const answer = new Array(n).fill(0);
+
+    const stack: number[] = [];
+
+    for (let i = 0; i < n; i++) {
+        // 当前温度比栈顶高的时候，处理栈顶元素
+        while (stack.length > 0 && temperatures[i] > temperatures[stack[stack.length - 1]]) {
+            const prevIndex = stack.pop();
+            if (prevIndex !== undefined) {
+                answer[prevIndex] = i - prevIndex
+            }
+        }
+        stack.push(i)
+    }
+    return answer
 };
 
 const temperatures = [73, 74, 75, 71, 69, 72, 76, 73]
