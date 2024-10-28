50712dc20cdcc8921c1b2ce871036005050e8dc1
cuishunbiao <xiang3399nian@gmail.com>
2024-10-28 13:00:13 +0800

    add code

--- src/sort-an-array.ts
+++ src/sort-an-array.ts
@@ -1,5 +1,18 @@
 function sortArray(nums: number[]): number[] {
-    return nums.sort((a, b) => a - b)
+    for (let i = 1; i < nums.length; i++) {
+        let key = nums[i];
+        let j = i - 1;
+
+        // 将大于 key 的元素移动到右面
+        while (j >= 0 && nums[j] > key) {
+            nums[j + 1] = nums[j]
+            j--
+        }
+
+        // 将 key 放到正确位置
+        nums[j + 1] = key;
+    }
+    return nums;
 };
 
 const nums222 = [5, 2, 3, 1]
