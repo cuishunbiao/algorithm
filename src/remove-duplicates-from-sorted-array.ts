4bdfdfcfcf260c9c5c4ce09791c94c1de3d62b14
cuishunbiao <xiang3399nian@gmail.com>
2024-11-19 13:36:34 +0800

    add code

--- src/remove-duplicates-from-sorted-array.ts
+++ src/remove-duplicates-from-sorted-array.ts
@@ -1,27 +1,50 @@
+/**
+ * let count = 1;
+ * let slow = 0;
+ * let fast = 0;
+ * 慢指针、快指针
+ * 
+ * 快指针，每次都移动一个下标；
+ *  判断，当前值和慢指针值，是否相等
+ *      
+ *      判断 count 是否大于 2；
+ *          大于：nums[slow] = nums[fast]
+ *          小于
+ *              判断是否相等
+ *                  相等，则slow+1;
+ *              如果不相等
+ *                  count+1;
+ * 
+ */
 function removeDuplicates1(nums: number[]): number {
+    // let count = 0;
+    // let slow = 0;
+
+    // for (let fast = 0; fast < nums.length; fast++) {
+    //     debugger
+    //     if (count >= 2) {
+    //         nums[slow] = nums[fast];
+    //     } else {
+    //         if (nums[slow] === nums[fast]) {
+    //             slow++;
+    //         } else {
+    //             count++;
+    //         }
+    //     }
+    // }
+    // return slow + 1;
+
+
     let slow = 0;
-    let count = 0;
-
     for (let fast = 0; fast < nums.length; fast++) {
-        if (nums[fast] === nums[slow]) {
-            count++;
-
+        if (slow < 2) {
+            // 前两个数字保留
+            nums[slow] = nums[fast];
+        } else if (nums[fast] !== nums[slow - 2]) {
+            // 当前数字与前两个数不相等
         }
-        // if (nums[fast] !== nums[slow]) {
-        //     count++;
-        // if (count === 2) {
-        //     slow++
-        //     nums[slow] = nums[fast];
-        // }
-        // } else {
-        //     count = 1;
-        // }
     }
-
-    console.log(nums);
-
-    return slow + 1;
-
+    return slow;
 };
 
 const nums0003 = [1, 1, 1, 2, 2, 3]
